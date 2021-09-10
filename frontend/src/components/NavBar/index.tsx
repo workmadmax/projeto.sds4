8eb4c4738b929a222013ccc7455c9eeb2e1c37a7
workmadmax <80357524+workmadmax@users.noreply.github.com>
2021-09-10 11:21:34 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,10 +1,13 @@
-import madmax42 from 'assets/img/madmax42.png'
+import madmax42 from "assets/img/madmax42.png";
+import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <div className="container">
         <nav className="my-2 my-md-0 mr-md-3">
-          <img src={madmax42} alt="madmax" width="120" />
+          <Link to="/">
+            <img src={madmax42} alt="madmax" width="120" />
+          </Link>
         </nav>
       </div>
     </div>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,29 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Page Home</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DsSales</h1>
+          <p className="lead">
+            Analyze your sales performance from different perspectives
+          </p>
+          <hr />
+          <p>
+            This application consists of displaying a dashboard from data
+            provided by a backend built with Spring Boot
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+            Access dashboard
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
